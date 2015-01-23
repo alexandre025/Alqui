152fdc769853916d441985a67fbdae7fc8111bea
Alexandre Ferraille <contact@alexandre-ferraille.fr>
2015-01-23 14:13:26 +0100

    Fix bug

--- app/models/app_model.php
+++ app/models/app_model.php
@@ -4,9 +4,9 @@ namespace APP\MODELS;
 
 class app_model {
 	
-	private var $dB;
+	private $dB;
 
-	function __constructor(){
+	function __construct(){
 		$f3=\Base::instance();
     	$this->dB=new \DB\SQL('mysql:host='.$f3->get('db_host').';port='.$f3->get('db_port').';dbname='.$f3->get('db_name'),$f3->get('db_login'),$f3->get('db_password'));
 	}
