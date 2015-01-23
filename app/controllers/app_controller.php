8df8d9180cfd7e4388d1df6cd9e13f19dbd7c17d
Alexandre Ferraille <contact@alexandre-ferraille.fr>
2015-01-23 14:00:40 +0100

    Namespace

--- app/controllers/app_controller.php
+++ app/controllers/app_controller.php
@@ -1,14 +1,22 @@
 <?php
 
+namespace APP\CONTROLLERS;
+
 class app_controller {
 
 	function __constructor(){
-
+    	$this->tpl='home.html';
+    	$this->model=new \APP\MODELS\app_model();
 	}
 
 	function home(){
-		echo View::instance()->render('home.html');
+
 	}
+
+	function afterroute(){
+    	echo \View::instance()->render($this->tpl);
+  	}
+
 }
 
 ?>
--- app/models/app_model.php
+++ app/models/app_model.php
@@ -1,11 +1,22 @@
 <?php
 
+namespace APP\MODELS;
+
 class app_model {
 	
 	private var $dB;
 
 	function __constructor(){
-		$this->dB = new DB\SQL('mysql:host=localhost;port=3306;dbname=location','location','location');
+		$f3=\Base::instance();
+    	$this->dB=new \DB\SQL('mysql:host='.$f3->get('db_host').';port='.$f3->get('db_port').';dbname='.$f3->get('db_name'),$f3->get('db_login'),$f3->get('db_password'));
+	}
+
+	private function getMapper($table){
+		return new \DB\SQL\Mapper($this->dB,$table);
+	}
+
+	public function log(){
+		return $this->dB->log();
 	}
 }
 
