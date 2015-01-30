5ac5572cc09e8a2de3dcf0ceb5a6d7de306936d4
alexandre025 <alexandre.ferraille@gmail.com>
2015-01-30 14:09:06 +0100

    set displayer.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,13 @@
 'use.strict'
 
 var $ = require ('./tools.js');
+var displayer = require('./ui/displayer.js');
 
 document.addEventListener('DOMContentLoaded',function(){
 
 
 
 
-
+displayer.init();
 
 },false);
