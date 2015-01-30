2ed25160881533b4bd77abd496aa447826e0e427
Alexandre Ferraille <contact@alexandre-ferraille.fr>
2015-01-30 14:43:14 +0100

    login popup

--- src/js/displayer.js
+++ src/js/displayer.js
@@ -5,6 +5,32 @@ var $ = require('../tools.js')
 var displayer = {
 
 	init : function(){
+		displayer.loginPopup();
+	},
 
+	loginPopup : function(){
+
+		// Click on login link
+		$.byId('login-link').addEventListener('click',function(e){
+			e.preventDefault();
+			$.byId('login-overlay').classList.add('active');
+		},false);
+
+		// Press escape 
+		window.addEventListener('keyup', function(e) {
+            if (e.keyCode === 38) {
+            	displayer.closeLogin();
+            }
+        },false);
+
+		// Click on overlay
+        $.byId('login-overlay').addEventListener('click',function(){
+        	displayer.closeLogin();
+        },false);
+
+	},
+	closeLogin : function(){
+		$.byId('login-overlay').classList.remove('active');
 	}
+
 }
