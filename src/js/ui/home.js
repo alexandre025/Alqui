e56ecf8965e5ac9ed3a535433f56e93cf4c6a170
Alexandre Ferraille <contact@alexandre-ferraille.fr>
2015-02-01 20:53:30 +0100

    count up

--- src/js/ui/home.js
+++ src/js/ui/home.js
@@ -1,7 +1,7 @@
 'use.strict'
 
 var $ = require('../tools.js');
-
+var countUp = require('../dependencies/countup.js');
 // Home specific JS 
 var home = {
 
@@ -10,34 +10,23 @@ var home = {
 	},
 	// Number animation
 	number : function(){
-		var once = false;
-		window.addEventListener('scroll',function(){
-			if(document.body.scrollTop > 150 && once == false){
-				once = true;
-				home.numbCount($.byClass('count-it'));				
-			}
-		},false);
+		home.numbCount($.byClass('count-it'));				
 	},
 	numbCount : function(elem){
-		var value0 = parseInt(elem[0].getAttribute('data-count'));
-		var value1 = parseInt(elem[1].getAttribute('data-count'));
-		var value2 = parseInt(elem[2].getAttribute('data-count'));
-		var max;
-		if(value0 >= value1 && value0 >= value2 ){ max = value0; }
-		else if(value1 >= value0 && value1 >= value2 ){ max = value1; }
-		else { max = value2; }
-		console.log(max);
-		for (var i = 0; i <= max; i++) {
-			if(value0>=i){set(elem[0],i,10000/value0);}
-			if(value1>=i){set(elem[1],i,10000/value1);}
-			if(value2>=i){set(elem[2],i,10000/value2);}
+		var value, count;
+		var options = {
+		  useEasing : true, 
+		  useGrouping : true, 
+		  separator : ' ', 
+		  decimal : '.', 
+		  prefix : '', 
+		  suffix : '' 
 		}
-		function set(elem,i,timer){
-			setTimeout(function(){
-				console.log(timer);
-				elem.innerHTML=i;
-			},timer);
-		}
+		for (var i = 0; i < elem.length; i++) {
+			value=parseInt(elem[i].getAttribute('data-count'));
+			count = new countUp(elem[i],0,value,0,10,options);
+			count.start();
+		};
 	} 
 };
 module.exports = home;
