e04b2678dca09bdb278a9ce1678c88b0a5beef7e
Alexandre Ferraille <contact@alexandre-ferraille.fr>
2015-02-06 14:49:01 +0100

    Account register

--- app/models/app_model.php
+++ app/models/app_model.php
@@ -9,6 +9,7 @@ class app_model {
 	function __construct(){
 		$f3=\Base::instance();
     	$this->dB=new \DB\SQL('mysql:host='.$f3->get('db_host').';port='.$f3->get('db_port').';dbname='.$f3->get('db_name'),$f3->get('db_login'),$f3->get('db_password'));
+    	$this->smtp=new \smtp($f3->get('smtp_host'),$f3->get('smtp_port'),$f3->get('smtp_scheme'),$f3->get('smtp_user'),$f3->get('smtp_password'));
 	}
 
 	private function getMapper($table){
