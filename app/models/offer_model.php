ca7c1f976480a8dd95fac025bef6573fa5aa609c
alexandre025 <alexandre.ferraille@gmail.com>
2015-02-07 21:16:51 +0100

    Add offer view

---
+++ app/models/offer_model.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace APP\MODELS;
+
+class offer_model {
+	
+	public $dB;
+	private $smtp;
+
+	function __construct(){
+		$f3=\Base::instance();
+    	$this->dB=new \DB\SQL('mysql:host='.$f3->get('db_host').';port='.$f3->get('db_port').';dbname='.$f3->get('db_name'),$f3->get('db_login'),$f3->get('db_password'));
+    	$this->smtp=new \smtp($f3->get('smtp_host'),$f3->get('smtp_port'),$f3->get('smtp_scheme'),$f3->get('smtp_user'),$f3->get('smtp_password'));
+	}
+
+	private function getMapper($table){
+		return new \DB\SQL\Mapper($this->dB,$table);
+	}
+
+	public function getCategories(){
+		return $this->dB->exec('SELECT * FROM category');
+	}
+
+	public function log(){
+		return $this->dB->log();
+	}
+
+}
+
+?>
