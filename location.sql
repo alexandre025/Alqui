4b11a71d88029ae13f78971b82ad8285af2f93e8
Alexandre Ferraille <contact@alexandre-ferraille.fr>
2015-02-19 11:44:58 +0100

    New database

--- location.sql
+++ location.sql
@@ -3,18 +3,12 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost:8889
--- Généré le :  Lun 09 Février 2015 à 17:57
+-- Généré le :  Jeu 19 Février 2015 à 11:44
 -- Version du serveur :  5.5.34
 -- Version de PHP :  5.5.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de données :  `location`
@@ -58,7 +52,14 @@ CREATE TABLE `comment` (
   `created_at` int(11) NOT NULL,
   `disabled_at` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+--
+-- Contenu de la table `comment`
+--
+
+INSERT INTO `comment` (`id`, `id_from`, `id_to`, `content`, `mark`, `created_at`, `disabled_at`) VALUES
+(7, 17, 16, 'Donnez une appréciation au loueur', 2, 1424263828, 0);
 
 -- --------------------------------------------------------
 
@@ -78,7 +79,16 @@ CREATE TABLE `offer` (
   `disabled_at` int(11) NOT NULL,
   `availability` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
+
+--
+-- Contenu de la table `offer`
+--
+
+INSERT INTO `offer` (`id`, `id_category`, `id_user`, `name`, `content`, `price_per_day`, `location`, `created_at`, `disabled_at`, `availability`) VALUES
+(14, 5, 16, 'Citroen C2', 'Pour que votre offre soit publié, ne divilguez pas vos coordonnées.\r\n        ', 125, 75002, 1423518814, 0, 2),
+(15, 9, 16, 'Chaussette', 'Pour que votre offre soit publié, ne divilguez pas vos coordonnées.\r\n        ', 5, 83200, 1423597681, 0, 1),
+(16, 4, 17, 'Nain de jardin', 'Pour que votre offre soit publié, ne divilguez pas vos coordonnées.\r\n        ', 15, 77410, 1424108870, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -93,7 +103,19 @@ CREATE TABLE `photo` (
   `created_at` int(11) NOT NULL,
   `disabled_at` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+
+--
+-- Contenu de la table `photo`
+--
+
+INSERT INTO `photo` (`id`, `id_offer`, `photo_name`, `created_at`, `disabled_at`) VALUES
+(6, 14, 'public/assets/offer_1423518814_Capture d’écran 2015-02-09 à 17.51.24.png', 1423518814, 0),
+(7, 14, 'public/assets/offer_1423518814_Capture d’écran 2015-02-09 à 22.07.34.png', 1423518814, 0),
+(8, 15, 'public/assets/offer_1423597681_1761049009.png', 1423597681, 0),
+(9, 15, 'public/assets/offer_1423597681_908559869.png', 1423597681, 0),
+(10, 16, 'public/assets/offer_1424108870_285535673.png', 1424108870, 0),
+(11, 17, 'public/assets/offer_1424108951_1137179708.png', 1424108951, 0);
 
 -- --------------------------------------------------------
 
@@ -103,14 +125,23 @@ CREATE TABLE `photo` (
 
 CREATE TABLE `reservation` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` int(1) NOT NULL,
   `id_offer` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `date_start` int(11) NOT NULL,
   `date_end` int(11) NOT NULL,
+  `commented` int(1) NOT NULL,
   `created_at` int(11) NOT NULL,
   `disabled_at` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `reservation`
+--
+
+INSERT INTO `reservation` (`id`, `status`, `id_offer`, `id_user`, `date_start`, `date_end`, `commented`, `created_at`, `disabled_at`) VALUES
+(1, 1, 14, 17, 1424247213, 1424353600, 1, 1424248345, 0);
 
 -- --------------------------------------------------------
 
@@ -133,8 +164,26 @@ CREATE TABLE `user` (
   `created_at` int(11) NOT NULL,
   `disabled_at` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Contenu de la table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `password`, `firstname`, `lastname`, `address`, `postal`, `city`, `country`, `mark`, `photo`, `created_at`, `disabled_at`) VALUES
+(16, 'alexandre.ferraille@gmail.com', '60c6d277a8bd81de7fdde19201bf9c58a3df08f4', 'Alexandre', 'Ferraille', '10 allée de la tournelle', '77410', 'Annet Sur Marne', 'France', 2, 'public/assets/profil_1423518672.png', 1423518655, 0),
+(17, 'max.fer@betatest.fr', '0706025b2bbcec1ed8d64822f4eccd96314938d0', 'Maxime', 'Ferraille', '', '', '', '', -1, '', 1423601470, 0),
+(18, 'j.cherain@gmail.com', '7fdfe229fce69a4d7f38653f3755ccbae5703f88', 'Juliette', 'Chérain', '', '', '', '', -1, '', 1423759433, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `wish`
+--
+
+CREATE TABLE `wish` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `id_offer` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=28 ;
