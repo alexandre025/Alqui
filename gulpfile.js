1cb435931dd50c0e1123c97336b479a04e7b35d1
Alexandre Ferraille <contact@alexandre-ferraille.fr>
2015-02-22 16:25:51 +0100

    Gulp file updated for admin js/sass

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,33 @@ var autoprefixer = require('gulp-autoprefixer');
 gulp.task('watch', function() {
   gulp.watch('src/js/**/*.js', ['js']);
   gulp.watch('src/sass/**/*.scss', ['sass']);
+  gulp.watch('src/admin/js/**/*.js', ['admin_js']);
+  gulp.watch('src/admin/sass/**/*.scss', ['admin_sass']);
 });
+
+// SCSS Admin Files
+gulp.task('admin_sass', function () {
+  gulp.src('./src/admin/sass/admin.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('./public/css'))
+});
+
+// JS Admin Files
+gulp.task('admin_js', function() {
+  gulp.src('src/admin/js/admin.js')
+    .pipe(browserify({
+      insertGlobals : true,
+      debug : true
+    }))
+    .pipe(gulp.dest('./public/js'))
+});
+
 
 // SCSS Files
 gulp.task('sass', function () {
@@ -32,4 +58,4 @@ gulp.task('js', function() {
     .pipe(gulp.dest('./public/js'))
 });
 
-gulp.task('default', ['js', 'sass', 'watch']);
+gulp.task('default', ['js', 'sass', 'admin_js', 'admin_sass','watch']);
