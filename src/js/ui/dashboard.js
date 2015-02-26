0ca1f1022fcd53f73f546b47ecfb17fa1d3e177c
Florian Cordillot <flo_9478@yahoo.fr>
2015-02-26 12:47:42 +0100

    Dashboard demandes

--- src/js/ui/dashboard.js
+++ src/js/ui/dashboard.js
@@ -36,6 +36,42 @@ var dashboard = {
 			_.byId('dashboard-reservations').style.display = 'none';
 			_.byId('dashboard-notifs').style.display = 'none';
 		},false);
+
+		var add_offer_button = _.byClass('add-offer-button');
+		for(var i = 0; i < add_offer_button.length; i++){
+			add_offer_button[i].addEventListener('click',function(){
+				_.byId('own-offer').style.display = 'none';
+				_.byId('add-offer').style.display = 'block';
+			},false);
+		}
+		_.byId('return-offer-location').addEventListener('click',function(){
+			_.byId('own-offer').style.display = 'block';
+			_.byId('add-offer').style.display = 'none';
+		},false);
+
+
+		_.byId('add-picture-1-fake').addEventListener('click',function(e){
+			e.preventDefault();
+			_.byId('add-picture-1').click();
+		},false);
+		_.byId('add-picture-2-fake').addEventListener('click',function(e){
+			e.preventDefault();
+			_.byId('add-picture-2').click();
+		},false);
+		_.byId('add-picture-3-fake').addEventListener('click',function(e){
+			e.preventDefault();
+			_.byId('add-picture-3').click();
+		},false);
+
+		_.byId('add-picture-1').addEventListener('change',function(){
+			_.byId('add-picture-1-fake').innerHTML = this.value.substr(12,this.value.length - 12);
+		},false);
+		_.byId('add-picture-2').addEventListener('change',function(){
+			_.byId('add-picture-2-fake').innerHTML = this.value.substr(12,this.value.length - 12);
+		},false);
+		_.byId('add-picture-3').addEventListener('change',function(){
+			_.byId('add-picture-3-fake').innerHTML = this.value.substr(12,this.value.length - 12);
+		},false);
 	}
 }
 module.exports = dashboard;
