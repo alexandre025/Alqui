62a6f32f634a227bd5e4b2e509813d2449ffd57d
fcordillot <florian.cordillot@gmail.com>
2015-03-06 00:25:36 +0100

    google maps

--- src/js/ui/offer.js
+++ src/js/ui/offer.js
@@ -7,6 +7,7 @@ var offer = {
 	init: function(){
 		offer.pickdate();
 		offer.sliderPhotos();
+		offer.addMap();
 	},
 	pickdate: function(){
 		$('.pickadate').pickadate({
@@ -33,6 +34,36 @@ var offer = {
 				_.byId('slider-view-photo').querySelector('img').src = this.querySelector('img').src;
 			},false);
 		}
+	},
+	addMap: function(){
+		var zip = _.byId('offer-location').getAttribute('data-location');
+		var loc = {};
+		var geocoder = new google.maps.Geocoder();
+		geocoder.geocode({'address': zip + ' France'},function(results,status){
+			if(status == google.maps.GeocoderStatus.OK){
+				loc.lat = results[0].geometry.location.lat();
+				loc.lng = results[0].geometry.location.lng();
+				loc.latLng = results[0].geometry.location;
+			}
+			else{
+				offer.response({success:false});
+			}
+
+			var center =  new google.maps.LatLng(loc.lat,loc.lng);	
+			var settings = {
+				zoom : 		12,
+				center : 	center,
+				mapTypeId : google.maps.MapTypeId.ROADMAP, 
+			};
+
+			this.map = new google.maps.Map(_.byId('offer-location'),settings);
+		
+			new google.maps.Marker({position:loc.latLng,map:this.map});
+			this.map.panTo(loc.latLng);
+		});
+	},
+	response: function(obj){
+		console.log(obj);
 	}
 }
 module.exports = offer;
